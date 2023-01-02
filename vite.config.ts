3a471e03a290bd1f50dddac071d54f87ac745977
lisonge <i@songe.li>
2023-01-02 19:23:47 +0800

    pref: polyfill

--- vite.config.ts
+++ vite.config.ts
@@ -2,17 +2,30 @@ import unocss from "unocss/vite";
 import { defineConfig } from "vite";
 import solid from "vite-plugin-solid";
 import tsconfigPaths from "vite-tsconfig-paths";
+import legacy from "@vitejs/plugin-legacy";
 import { solidSvg } from "./plugins";
 
 export default defineConfig(({ command }) => {
+  const cmdConfig = <T1, T2>(serve: T1, build: T2) => {
+    return command == "build" ? build : serve;
+  };
   return {
-    plugins: [solid(), solidSvg(), unocss(), tsconfigPaths()],
+    plugins: [
+      solid(),
+      solidSvg(),
+      unocss(),
+      tsconfigPaths(),
+      legacy({
+        renderLegacyChunks: false,
+        modernPolyfills: true,
+      }),
+    ],
     server: {
       port: 8051,
       host: `0.0.0.0`,
     },
     build: {
-      target: "esnext",
+      target: cmdConfig("esnext", "chrome80"),
     },
   };
 });
